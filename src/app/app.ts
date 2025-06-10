f901241357d679961970d97c091454ce22333b71
Avinash-zeus <Avinash.Singh@zeuslearning.com>
2025-06-10 13:12:43 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginHeader } from './login-page/login-header/login-header';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'quantum';
+
 }
